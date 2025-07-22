f308c1805f506a295b45eda1088bd9837da52d67
Derozen <manbationo@gmail.com>
2025-07-22 11:53:38 +0100

    scrap

--- scrap.py
+++ scrap.py
@@ -7,9 +7,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import random
-import pandas as pd
-
-df = pd.read_csv("burkina.csv")
 
 load_dotenv()
 
